890d6fdd7645e10ead91bd4c13b54a2d91b341d1
Anders Furseth <anders.furseth@gmail.com>
2018-11-13 21:36:25 +0100

    Add ZoneRulesException

--- src/jiffy/zone/zone_rules_exception.cljs
+++ src/jiffy/zone/zone_rules_exception.cljs
@@ -1,8 +1,7 @@
-(ns jiffy.zone.zone-rules-exception
-  (:require [jiffy.dev.wip :refer [wip]]
-            [jiffy.date-time-exception :as DateTimeException]))
-
-(defrecord ZoneRulesException [])
+(ns jiffy.zone.zone-rules-exception)
 
 ;; FIXME: no implementation found from inherited class class java.time.DateTimeException
-
+(defn ZoneRulesException
+  ([s] (ZoneRulesException s nil nil))
+  ([s m] (ZoneRulesException s m nil))
+  ([s m e] (ex-info s (or m {}))))
