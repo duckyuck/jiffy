68aa725bbf2709659f5381d708929f037af5d43e
Anders Furseth <anders.furseth@gmail.com>
2018-11-15 18:24:21 +0100

    Load all ns'es in main to detect problems across the board

--- src/jiffy/dev/main.cljs
+++ src/jiffy/dev/main.cljs
@@ -1,14 +1,187 @@
 (ns java-time-clj.main
-  (:require [jiffy.duration :as Duration]
+  (:require jiffy.big-decimal
+            jiffy.chrono.abstract-chronology
+            jiffy.chrono.chrono-local-date
+            jiffy.chrono.chrono-local-date-impl
+            jiffy.chrono.chrono-local-date-time
+            jiffy.chrono.chrono-local-date-time-impl
+            jiffy.chrono.chronology
+            jiffy.chrono.chrono-period
+            jiffy.chrono.chrono-period-impl
+            jiffy.chrono.chrono-zoned-date-time
+            jiffy.chrono.chrono-zoned-date-time-impl
+            jiffy.chrono.era
+            jiffy.chrono.hijrah-chronology
+            jiffy.chrono.hijrah-date
+            jiffy.chrono.hijrah-era
+            jiffy.chrono.iso-chronology
+            jiffy.chrono.iso-era
+            jiffy.chrono.japanese-chronology
+            jiffy.chrono.japanese-date
+            jiffy.chrono.japanese-era
+            jiffy.chrono.minguo-chronology
+            jiffy.chrono.minguo-date
+            jiffy.chrono.minguo-era
+            jiffy.chrono.thai-buddhist-chronology
+            jiffy.chrono.thai-buddhist-date
+            jiffy.chrono.thai-buddhist-era
+            jiffy.clock
+            jiffy.date-time-exception
+            jiffy.day-of-week
+            jiffy.dev.wip
+            [jiffy.duration :as Duration]
+            jiffy.format.date-time-formatter
+            jiffy.format.date-time-formatter-builder
+            jiffy.format.date-time-parse-context
+            jiffy.format.date-time-parse-exception
+            jiffy.format.date-time-print-context
+            jiffy.format.date-time-text-provider
+            jiffy.format.decimal-style
+            jiffy.format.format-style
+            jiffy.format.parsed
+            jiffy.format.resolver-style
+            jiffy.format.sign-style
+            jiffy.format.text-style
             [jiffy.instant :as Instant]
+            jiffy.local-date
+            jiffy.local-date-time
+            jiffy.local-time
+            jiffy.math
+            jiffy.month
+            jiffy.month-day
+            jiffy.offset-date-time
+            jiffy.offset-time
+            jiffy.period
+            jiffy.system-clock
             [jiffy.temporal.chrono-field :as ChronoField]
-            [jiffy.temporal.temporal-accessor :as TemporalAccessor]))
+            jiffy.temporal.chrono-unit
+            jiffy.temporal.iso-fields
+            jiffy.temporal.julian-fields
+            jiffy.temporal.temporal
+            [jiffy.temporal.temporal-accessor :as TemporalAccessor]
+            jiffy.temporal.temporal-adjuster
+            jiffy.temporal.temporal-adjusters
+            jiffy.temporal.temporal-amount
+            jiffy.temporal.temporal-field
+            jiffy.temporal.temporal-queries
+            jiffy.temporal.temporal-query
+            jiffy.temporal.temporal-unit
+            jiffy.temporal.unsupported-temporal-type-exception
+            jiffy.temporal.value-range
+            jiffy.temporal.week-fields
+            jiffy.time-comparable
+            jiffy.year
+            jiffy.year-impl
+            jiffy.year-month
+            jiffy.zoned-date-time
+            jiffy.zone-id
+            jiffy.zone-offset
+            jiffy.zone-region
+            jiffy.zone.tzdb-zone-rules-provider
+            jiffy.zone.zone-offset-transition
+            jiffy.zone.zone-offset-transition-rule
+            jiffy.zone.zone-rules
+            jiffy.zone.zone-rules-exception
+            jiffy.zone.zone-rules-provider))
+
+
+(comment
+  jiffy.big-decimal/keep-me
+  jiffy.chrono.abstract-chronology/keep-me
+  jiffy.chrono.chrono-local-date/keep-me
+  jiffy.chrono.chrono-local-date-impl/keep-me
+  jiffy.chrono.chrono-local-date-time/keep-me
+  jiffy.chrono.chrono-local-date-time-impl/keep-me
+  jiffy.chrono.chrono-period/keep-me
+  jiffy.chrono.chrono-period-impl/keep-me
+  jiffy.chrono.chrono-zoned-date-time/keep-me
+  jiffy.chrono.chrono-zoned-date-time-impl/keep-me
+  jiffy.chrono.chronology/keep-me
+  jiffy.chrono.era/keep-me
+  jiffy.chrono.hijrah-chronology/keep-me
+  jiffy.chrono.hijrah-date/keep-me
+  jiffy.chrono.hijrah-era/keep-me
+  jiffy.chrono.iso-chronology/keep-me
+  jiffy.chrono.iso-era/keep-me
+  jiffy.chrono.japanese-chronology/keep-me
+  jiffy.chrono.japanese-date/keep-me
+  jiffy.chrono.japanese-era/keep-me
+  jiffy.chrono.minguo-chronology/keep-me
+  jiffy.chrono.minguo-date/keep-me
+  jiffy.chrono.minguo-era/keep-me
+  jiffy.chrono.thai-buddhist-chronology/keep-me
+  jiffy.chrono.thai-buddhist-date/keep-me
+  jiffy.chrono.thai-buddhist-era/keep-me
+  jiffy.clock/keep-me
+  jiffy.date-time-exception/keep-me
+  jiffy.day-of-week/keep-me
+  jiffy.dev.wip/keep-me
+  jiffy.duration/keep-me
+  jiffy.format.date-time-formatter
+  jiffy.format.date-time-formatter-builder/keep-me
+  jiffy.format.date-time-parse-context/keep-me
+  jiffy.format.date-time-parse-exception/keep-me
+  jiffy.format.date-time-print-context/keep-me
+  jiffy.format.date-time-text-provider/keep-me
+  jiffy.format.decimal-style/keep-me
+  jiffy.format.format-style/keep-me
+  jiffy.format.parsed/keep-me
+  jiffy.format.resolver-style/keep-me
+  jiffy.format.sign-style/keep-me
+  jiffy.format.text-style/keep-me
+  jiffy.instant/keep-me
+  jiffy.local-date/keep-me
+  jiffy.local-date-time/keep-me
+  jiffy.local-time/keep-me
+  jiffy.math/keep-me
+  jiffy.month/keep-me
+  jiffy.month-day/keep-me
+  jiffy.offset-date-time/keep-me
+  jiffy.offset-time/keep-me
+  jiffy.period/keep-me
+  jiffy.system-clock/keep-me
+  jiffy.temporal.chrono-field/keep-me
+  jiffy.temporal.chrono-unit/keep-me
+  jiffy.temporal.iso-fields/keep-me
+  jiffy.temporal.julian-fields/keep-me
+  jiffy.temporal.temporal/keep-me
+  jiffy.temporal.temporal-accessor/keep-me
+  jiffy.temporal.temporal-adjuster/keep-me
+  jiffy.temporal.temporal-adjusters/keep-me
+  jiffy.temporal.temporal-amount/keep-me
+  jiffy.temporal.temporal-field/keep-me
+  jiffy.temporal.temporal-queries/keep-me
+  jiffy.temporal.temporal-query/keep-me
+  jiffy.temporal.temporal-unit/keep-me
+  jiffy.temporal.unsupported-temporal-type-exception/keep-me
+  jiffy.temporal.value-range/keep-me
+  jiffy.temporal.week-fields/keep-me
+  jiffy.time-comparable/keep-me
+  jiffy.year/keep-me
+  jiffy.year-impl/keep-me
+  jiffy.year-month/keep-me
+  jiffy.zone.tzdb-zone-rules-provider/keep-me
+  jiffy.zone.zone-offset-transition/keep-me
+  jiffy.zone.zone-offset-transition-rule/keep-me
+  jiffy.zone.zone-rules/keep-me
+  jiffy.zone.zone-rules-exception/keep-me
+  jiffy.zone.zone-rules-provider/keep-me
+  jiffy.zone-id/keep-me
+  jiffy.zone-offset/keep-me
+  jiffy.zone-region/keep-me
+  jiffy.zoned-date-time/keep-me)
 
 (enable-console-print!)
 
-(let [a (Instant/now)
-      b (Instant/plusSeconds a 60)]
-  (Duration/between a b))
+(comment
 
-(TemporalAccessor/getLong (Instant/now) ChronoField/INSTANT_SECONDS)
+  (def i1 (Instant/now))
+  (def i2 (-> i1
+              (Instant/plusSeconds 30)
+              (Instant/plusMillis 2500)))
+  (def d1 (Duration/between i1 i2))
+
+  (Duration/toHoursPart d1)
+
+)
 
