647ca1db7b492e05e386c2e9822a562df798f5d6
Anders Furseth <anders.furseth@gmail.com>
2018-11-28 03:04:25 +0100

    Reduce number of `math` parity checks
    For reduced test run time for now

--- test/jiffy/parity_tests/math_test.clj
+++ test/jiffy/parity_tests/math_test.clj
@@ -13,27 +13,27 @@
 ;; manifest itself in failing API parity tests.
 (s/def ::floor-int (s/int-in -2147483647 2147483647))
 
-(defspec floor-div-test 100000
+(defspec floor-div-test 10000
   (prop/for-all [[x y] (s/gen (s/tuple ::floor-int (s/and ::floor-int (comp not zero?))))]
                 (same? (sut/floor-div x y)
                        (Math/floorDiv x y))))
 
-(defspec floor-mod-test 100000
+(defspec floor-mod-test 10000
   (prop/for-all [[x y] (s/gen (s/tuple ::floor-int (s/and ::floor-int (comp not zero?))))]
                 (same? (sut/floor-mod x y)
                        (Math/floorMod x y))))
 
-(defspec add-exact-test 100000
+(defspec add-exact-test 10000
   (prop/for-all [[x y] (s/gen (s/tuple int? int?))]
                 (same? (sut/add-exact x y)
                        (Math/addExact x y))))
 
-(defspec subtract-exact-test 100000
+(defspec subtract-exact-test 10000
   (prop/for-all [[x y] (s/gen (s/tuple int? int?))]
                 (same? (sut/subtract-exact x y)
                        (Math/subtractExact (long x) (long y)))))
 
-(defspec multiply-exact-test 100000
+(defspec multiply-exact-test 1000
   (prop/for-all [[x y] (s/gen (s/tuple int? int?))]
                 (same? (sut/multiply-exact x y)
                        (Math/multiplyExact x y))))
