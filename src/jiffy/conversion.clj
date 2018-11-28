981b472c5457d710de1f8a080aefc88e86acf692
Anders Furseth <anders.furseth@gmail.com>
2018-11-28 03:05:18 +0100

    Throw sensible exception from conversion multimethods default impls

--- src/jiffy/conversion.clj
+++ src/jiffy/conversion.clj
@@ -1,7 +1,17 @@
 (ns jiffy.conversion)
 
 (defmulti jiffy->java type)
-(defmethod jiffy->java :default [jiffy-object] jiffy-object)
+(defmethod jiffy->java :default
+  [jiffy-object]
+  (if (record? jiffy-object)
+    (throw (ex-info (str "Shit! We're missing implementation of multimethod 'jiffy.conversion/jiffy->java' for record "
+                         (type jiffy-object) ". Please do some more programming!") {:record jiffy-object}))
+    jiffy-object))
 
 (defmulti same? (fn [jiffy-object java-object] (type jiffy-object)))
-(defmethod same? :default [jiffy-object java-object] (= jiffy-object java-object))
+(defmethod same? :default
+  [jiffy-object java-object]
+  (if (record? jiffy-object)
+    (throw (ex-info (str "Darnit! We're missing implementation of multimethod 'jiffy.conversion/same?' for record "
+                         (type jiffy-object) ". Please do some more programming!") {:record jiffy-object}))
+    (= jiffy-object java-object)))
