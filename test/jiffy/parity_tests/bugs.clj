e0d67befd3b9f18255749e8d7a7ce2065cb505e9
Anders Furseth <anders.furseth@gmail.com>
2019-06-11 14:12:29 +0200

    Fix compilation error when launching clj repl

--- test/jiffy/parity_tests/bugs.clj
+++ test/jiffy/parity_tests/bugs.clj
@@ -1,10 +1,9 @@
 (ns jiffy.parity-tests.bugs
   (:require [clojure.test :refer [deftest is]]
             [jiffy.conversion :refer [jiffy->java]]
-            [jiffy.instant-impl])
-  (:import [jiffy.instant_impl Instant]))
+            [jiffy.instant-impl :as instant-impl]))
 
 (deftest java-toEpochMilli-bug
   (is (thrown-with-msg?
        java.lang.ArithmeticException #"long overflow"
-       (.toEpochMilli (jiffy->java #jiffy.instant_impl.Instant{:seconds 9223372036854776, :nanos 100000})))))
+       (.toEpochMilli (jiffy->java (instant-impl/create 9223372036854776 100000))))))
